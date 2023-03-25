ef308eda7fc961d8ec065941030401e02044500c
gowestmen <126822329+gowestmen@users.noreply.github.com>
2023-03-25 23:17:38 +0100

    mo

--- tilesets/Fond.tsx
+++ tilesets/Fond.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Fond" tilewidth="32" tileheight="32" tilecount="696" columns="29">
- <image source="E:/cours/Atelier pro/campusentier.JPG" width="928" height="798"/>
+ <image source="campusentier.JPG" width="928" height="798"/>
 </tileset>
