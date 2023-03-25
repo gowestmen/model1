d69716d138052967113a8426a289e8b43681feeb
gowestmen <126822329+gowestmen@users.noreply.github.com>
2023-03-25 23:51:00 +0100

    jklm

---
+++ tilesets/fond_ext.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="fond_ext" tilewidth="32" tileheight="32" tilecount="1144" columns="44">
+ <image source="testcampus_3d_imagesat.PNG" width="1413" height="854"/>
+</tileset>
