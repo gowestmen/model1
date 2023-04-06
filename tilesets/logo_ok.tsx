cf8ee1711f9042935c886543cf75b3dd8dc284f1
gowestmen <126822329+gowestmen@users.noreply.github.com>
2023-04-06 18:44:55 +0200

    dsfvfbf

---
+++ tilesets/logo_ok.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="logo_ok" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="sigat_Plan de travail 1.png" width="34" height="34"/>
+</tileset>
