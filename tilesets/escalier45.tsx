aa21ba2c154c2545dc0ed99a2bb8bde907f78c8c
gowestmen <126822329+gowestmen@users.noreply.github.com>
2023-04-06 18:25:37 +0200

    ajout batl + modif logo de tp

---
+++ tilesets/escalier45.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="escalier45" tilewidth="32" tileheight="32" tilecount="3132" columns="54">
+ <image source="escalier45_2.png" width="1742" height="1871"/>
+</tileset>
---
+++ tilesets/fd_batl.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="fd_batl" tilewidth="32" tileheight="32" tilecount="7396" columns="86">
+ <image source="fd_batl-01.jpg" width="2755" height="2758"/>
+</tileset>
