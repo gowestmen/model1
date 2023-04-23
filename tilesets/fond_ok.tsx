b69c6a56209c93b3f36a697138b3ebe8b323bddd
gowestmen <126822329+gowestmen@users.noreply.github.com>
2023-04-23 15:14:08 +0200

    nouveau plan

---
+++ tilesets/fond_ok.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="fond_ok" tilewidth="32" tileheight="32" tilecount="5032" columns="68">
+ <image source="fond-01.png" width="2203" height="2370"/>
+</tileset>
