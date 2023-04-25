0711fbca3b5c8fa82510a4e181c449a972282d40
gowestmen <126822329+gowestmen@users.noreply.github.com>
2023-04-25 22:39:07 +0200

    lvl 3

---
+++ tilesets/WA_user_interface.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="WA_user_interface" tilewidth="32" tileheight="32" tilecount="273" columns="13">
+ <image source="WA_User_Interface.png" width="416" height="672"/>
+</tileset>
